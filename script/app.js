328f790587e8350081c64500aa6c228806ca071f
PaulineOndiek <paulineondiek76@gmail.com>
2022-08-31 12:25:04 +0300

    designed the user interface and started working on the JavaSript part

---
+++ script/app.js
@@ -0,0 +1,125 @@
+console.log("Hello World");
+class calculation{
+    constructor(first, second)
+    this.first=first
+    this.second=second
+    this.clear()
+}
+
+const first=document.querySelector("[data-first]")
+console.log(first)
+const second=document.querySelector("[data-second]")
+console.log(second);
+const numerics=document.querySelectorAll("[data-number]");
+console.log(numerics);
+const operators=document.querySelectorAll("[data-operation]");
+console.log(operators);
+const clear=document.querySelector("[data-clear]")
+console.log(clear);
+const equal=document.querySelector("[data-equal]")
+console.log(equal)
+
+clear(),{
+this.first=""
+this.second=""
+this.clear="undefined"
+}
+
+
+appendNumber(number),{
+
+
+}
+
+selectOperation(operation),{
+
+
+}
+
+compute(),{
+
+
+}
+
+
+
+const calculation= new calculation(text, )
+clear (), {
+
+}
+
+appendNumber(number), {
+
+}
+
+selectOperation(operation), {
+
+
+}
+
+compute (), {
+
+}
+
+updateDisplay(), {
+
+}
+
+numerics.forEach(button=>{
+button.addEventListener("click", () => {
+calculation.appendNumber(button.innerText)
+calculation.updateDisplay 
+} )
+})
+
+operators.forEach(button=>{
+    button.addEventListener("click", ()=>{
+      calculation.selectOperation(button.innerText)  
+      calculation.updateDisplay
+    })
+})
+
+clear.addEventListener("click", button=>{
+calculation.clear(button.innerText)
+calculation.updatedDisplay
+
+    }
+
+    
+)
+
+equal.addEventListener(
+  "click", button=>{
+    calculation.equal(button.innerText)
+    calculation.updateDisplay
+
+  }  
+)
+
+
+
+
+
+// let answer;
+
+//                                                                 equal.addEventListener(
+//                                                                     "click", function (event) {
+//                                                                         text.value=event.target.textContent
+//                                                                     }
+//                                                                     )
+//                                                                     clear.addEventListener(
+//                                                                         "click", function (event) {
+//                                                                             text.value=event.target.textContent
+//                                                                         }
+//                                                                         )
+//                                                                         decimal.addEventListener(
+//                                                                             "click", function (event) {
+//                                                                                 text.value=event.target.textContent
+//                                                                             }
+//                                                                             )
+
+                                                                                            
+                                                                                                    
+                                                                                                        
+                                                                                                
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
